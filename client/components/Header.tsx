22637d9044242a12f0562732d0367c920a869607
Max Weiner <maxweiner02@gmail.com>
2023-09-26 11:17:19 -0600

    refactored for grid, added MUI icons and basic nav

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
-import { Button } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import { IconButton } from '@mui/material';
 
+// This interface needs to be changed if any new props are going to be passed in
+interface HeaderProps {
+  open: boolean,
+  openDrawer: Function
+}
 
-function Header () {
+function Header (props: HeaderProps) {
   return (
     <div className="header">
-      <h1>This is Header</h1>
+      {/* The onClick function opens/closes a collapsing sidebar, it can be ignored if using a static one */}
+      <IconButton size='large' onClick={() => props.openDrawer(!props.open)} id='menu_icon'>
+        <MenuIcon sx={{ color: 'white'}} fontSize='large'/>
+      </IconButton>
+      <h1 id='title'>moat</h1>
+      <IconButton size='large' id='bell_icon'>
+        <NotificationsIcon sx={{ color: 'white'}} fontSize='large'/>
+      </IconButton>
     </div>
   )
 }
--- client/components/SideNav.tsx
+++ client/components/SideNav.tsx
@@ -1,11 +1,40 @@
 import React from 'react';
-import { Button } from '@mui/material';
+import { ListItem, ListItemButton, ListItemText } from '@mui/material';
+
+interface NavProps {
+  open: boolean,
+  openDrawer: Function
+}
+
+/* 
+This is an MUI component which will create a sidebar that pops up on the left of the screen,
+it can be closed by clicking outside of it.
+
+The 'open' and 'openDrawer' props are the state which controls whether the sidebar is visible or not
+** They can be ignored if not using a collapsing sidebar **
+
+  <Drawer PaperProps={{ sx: { width: '25%' }}} open={props.open} onClose={() => props.openDrawer(!props.open)}>
+    <ListItem>
+      <ListItemText>Hello</ListItemText>
+    </ListItem>
+  </Drawer>
+
+*/
 
 
-function SideNav () {
+function SideNav (props: NavProps) {
   return (
     <div className="sideNav">
-      <h1>This is SideNav</h1>
+      <ListItemButton>
+        <ListItemText>About</ListItemText>
+      </ListItemButton>
+      <ListItemButton>
+        <ListItemText>Logs</ListItemText>
+      </ListItemButton>
+      {/* Adding onClick functionality goes on the ListItemButton, any icons get added as nested components in the ListItemButton */}
+      <ListItemButton>
+        <ListItemText>More Buttons</ListItemText>
+      </ListItemButton>
     </div>
   )
 }
