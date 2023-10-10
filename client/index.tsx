21f6946e0805d6d3a3a2b25c3569d0e66315f159
Meredith Britt - Mac <118227496+mfrazb@users.noreply.github.com>
2023-10-10 09:32:26 -0400

    Update front end to use React Router to serve pages

--- client/index.tsx
+++ client/index.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
+import ErrorPage from './routes/ErrorPage';
+import MainDashboard from './containers/MainDashboard';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
 // with TS, check if root element exists before rendering
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Failed to find the root element');
 const root = createRoot(rootElement);
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+);
