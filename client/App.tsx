d9064b2a041cfd8eb5f339cc41b5d0d65dad91e4
Akon530 <Akon530@users.noreply.github.com>
2023-10-10 14:52:56 -0400

    added navigation state to dashboard

--- client/App.tsx
+++ client/App.tsx
@@ -4,6 +4,9 @@ import { getStyle } from './scss/styles.scss';
 import Header from './components/Header';
 import SideNav from './components/SideNav';
 import Footer from './components/Footer';
+import Menu from './components/Menu';
+import NodeGraphContainer from './containers/NodeGraphContainer';
+import LogsContainer from './containers/LogsContainer';
 
 const App: React.FC = () => {
   // REMEMBER: When passing state using TypeScript you need to setup an interface in the child component with the format of the props you want to pass in
@@ -12,18 +15,34 @@ const App: React.FC = () => {
   // render page based on state
   const [open, openDrawer] = useState<boolean>(false);
 
+  //Page navigation state
+  const [selection, menuSelect] = useState<string>('main');
+
   const toggleDrawer = (event: MouseEvent) => {
     if (open === false) openDrawer(true);
     if (open === true) openDrawer(false);
   };
 
+  //Decides which page to render
+  let page;
+  switch(selection){
+    case 'main':
+      page = <MainDashboard open={open} />
+      break;
+    case 'node':
+      page = <NodeGraphContainer open={open} />
+      break;
+    case 'logs':
+      page = <LogsContainer open={open} />
+      break;
+  }
+
   return (
     <div style={getStyle} id='origin'>
       <Header open={open} toggleDrawer={toggleDrawer} />
+      <Menu menuSelect={menuSelect}/>
       <SideNav open={open} toggleDrawer={toggleDrawer} />
-      <MainDashboard open={open} />
-      {/* Log Dashboard */}
-      {/* Node Graph Dashboard */}
+      {page}
       <Footer open={open} />
     </div>
   );
